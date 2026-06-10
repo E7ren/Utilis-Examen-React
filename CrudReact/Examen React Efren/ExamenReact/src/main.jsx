10b2fdf0eb9e3908662cc060e5329c2e6202852c
Efren <efrenmonsonis28@gmail.com>
2026-06-10 12:42:01 +0200

    Preparacio examen react

--- CrudReact/Examen React Efren/ExamenReact/src/main.jsx
+++ CrudReact/Examen React Efren/ExamenReact/src/main.jsx
@@ -1,10 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import {RouterProvider} from "react-router-dom"
+import Router from './Router';
+import { AuthProvider } from './auth/AuthContext';
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <AuthProvider>
+      <RouterProvider route={Router} />
+    </AuthProvider>
   </StrictMode>,
 )
